fd15f700b7425b974ac2b222f464336b192f865c
Fitonho <fitonho@gmail.com>
2020-07-20 21:49:28 -0300

    erro de escrita inválida na linha 136 corrigido. variável firstArg agora se chama nextArg

--- myBash.c
+++ myBash.c
@@ -5,54 +5,48 @@
 #include <string.h>
 #include <signal.h>
 
-#define MAX_CMD_SIZE 512
-#define MAX_ARG_NUMBER 10
+//altere esses limites se necessário.
+#define MAX_INPUT_SIZE 1024
+#define MAX_ARG_COUNT 20
 
 void printPromptString(); //cria e printa a Prompt String
 char* parseCWD(); //se cwd for filho ou o próprio $HOME, coloca '~' no lugar do $HOME
-char* getCommand(char* command,unsigned long size); //retorna stdin menos \n
-char** split(char* str,char** splices); //divide str em substrings separadas por ' ' 
-int runCommand(char* command, char** argList); //roda o comando se for um built-in, senão chama spawn. retorna 1 caso comando seja exit
-int spawn(char* program, char** argList,int readPipe,int writePipe); //spawna um processo do programa de entrada
-void ignore (int signal_number);
-void sigStructsInit();
+void getCommand(char* command,unsigned long size); //retorna stdin menos \n
+void split(char* str,char** splices); //divide str em substrings separadas por ' ' 
+int runCommand(char* command, char** argList); //roda o comando se for um built-in, senão chama spawnNextCommand.
+int spawn(char* program, char** argList,int readPipe,int writePipe); //spawna o comando enviado com os pipes corretos
+void ignore (int signal_number); //função enviada para as estruturas de captura de sinal
+void initializeSignalStructures();
 int changeDirectory(char* command, char** argList);
-int getNextPipePosition(char** argList);
-int handlePipes(char* command, char** argList, int readPipe);
+int getNextPipePosition(char** argList); //retorna a posição do próximo pipe em argList. retorna 0 se não houver nenhum
+int spawnNextCommand(char* command, char** argList, int readPipe); //cria os pipes necessários e spawna todos os comandos recursivamente
+void waitAllChilds();
+void cleanArgList(char* command, char** argList);
 
 char* user;
 char* name;
 char* home;
 sig_atomic_t interrupted = 0;
 int running = 1;
-int childList[MAX_ARG_NUMBER];
+int childList[MAX_ARG_COUNT];
 int childI = 0;
 
-
-
 int main(){
-    sigStructsInit();
+    initializeSignalStructures();
     user = getenv("USER");
     name = getenv("NAME");
     home = getenv("HOME");
     int childStatus;
-    char* command = (char*)malloc(MAX_CMD_SIZE * sizeof(char));
-    char** argList = malloc(MAX_ARG_NUMBER * sizeof(char*));
+    char* command = (char*)malloc(MAX_INPUT_SIZE * sizeof(char));
+    char** argList = malloc(MAX_ARG_COUNT * sizeof(char*));
 
     while(running){
-        
         printPromptString();
-        command = getCommand(command,MAX_CMD_SIZE);
-        argList = split(command,argList);
-        if(runCommand(argList[0],argList)) break;
-        for(int i = 0; i<childI;i++){
-            waitpid(childList[i],&childStatus,0);
-        }
-        childI =0;
-        for(int i=0; argList[i]!=NULL;i++){
-            argList[i] = NULL;
-        }
-        command[0]='\0';
+        getCommand(command,MAX_INPUT_SIZE);
+        split(command,argList);
+        runCommand(argList[0],argList);
+        waitAllChilds();
+        cleanArgList(command,argList);
     }
 
     free(argList);
@@ -71,22 +65,7 @@ void printPromptString(){
     free(cwd);
 }
 
-char* parseCWD(){
-    char* cwd = getcwd(NULL,0);
-    char* parsedCwd = malloc(strlen(cwd));
-    if(strncmp(cwd,home,strlen(home)) == 0){
-        parsedCwd[0]='~';
-        strcpy(parsedCwd+1,cwd+strlen(home));
-        free(cwd);
-        return parsedCwd;
-    }
-    else{
-        free(parsedCwd);
-        return cwd;
-    }    
-}
-
-char* getCommand(char* command,unsigned long size){
+void getCommand(char* command,unsigned long size){
     fgets(command,size,stdin);
     if(strlen(command)>0)
         command[strlen(command)-1] = '\0';
@@ -97,22 +76,19 @@ char* getCommand(char* command,unsigned long size){
         printf("\n");
         interrupted = 0;
     }
-        
-    return command;
 }
 
-char** split(char* str,char** splices){
+void split(char* str,char** splices){
     char delim = ' ';
     int i;
     
     char* token = strtok(str,&delim);
-    for(i=0; token!=NULL && i<MAX_ARG_NUMBER;i++){
+    for(i=0; token!=NULL && i<MAX_ARG_COUNT;i++){
         splices[i] = token;
         token = strtok(NULL,&delim);
     }
     splices[i]=NULL;
     free(token);
-    return splices;
 }
 
 int runCommand(char* command, char** argList){
@@ -122,51 +98,52 @@ int runCommand(char* command, char** argList){
     if(strcmp(command,"cd") == 0)
         changeDirectory(command,argList);
         
-    else if(strcmp(command,"exit") == 0)
+    else if(strcmp(command,"exit") == 0){
+        running = 0;
         return 1;
+    }
     else{
-        handlePipes(command,argList,0);
+        spawnNextCommand(command,argList,0);
     }
     
     return 0;
 }
-int handlePipes(char* command, char** argList,int readPipe){
+
+void waitAllChilds(){
+    int childStatus;
+    for(int i = 0; i<childI;i++){
+        waitpid(childList[i],&childStatus,0);
+    }
+    childI =0;
+}
+
+void cleanArgList(char* command, char** argList){
+    for(int i=0; argList[i]!=NULL;i++){
+        argList[i] = NULL;
+    }
+    command[0]='\0';
+}
+
+int spawnNextCommand(char* command, char** argList,int readPipe){
     int pipePos;
     if((pipePos = getNextPipePosition(argList)) != 0){
         int pipeDescriptors[2];
         pipe(pipeDescriptors);
         int readFd = pipeDescriptors[0]; int writeFd = pipeDescriptors[1];
 
-        char** firstArg = malloc(pipePos*sizeof(char*));
-        memcpy(firstArg,argList,pipePos*sizeof(char*));
-        firstArg[pipePos] = NULL;
+        char** nextArg = malloc((pipePos+1)*sizeof(char*));
+        memcpy(nextArg,argList,pipePos*sizeof(char*));
+        nextArg[pipePos] = NULL;
 
-        spawn(command,firstArg,readPipe,writeFd);
+        spawn(command,nextArg,readPipe,writeFd);
         close(writeFd);
-        handlePipes(argList[pipePos+1],&argList[pipePos+1],readFd);
+        spawnNextCommand(argList[pipePos+1],&argList[pipePos+1],readFd);
         close(readFd);
-        free(firstArg);
+        free(nextArg);
     }else{
         spawn(command,argList,readPipe,0);
     }
 }
-
-int getNextPipePosition(char** argList){
-    for(int i=0; argList[i]!=NULL;i++){
-            if(strcmp(argList[i],"|") == 0){
-                return i;
-            }
-    }
-    return 0;
-}
-
-int changeDirectory(char* command, char** argList){
-    if( argList[1] == NULL || strcmp(argList[1],"~") == 0)
-        chdir(home);
-    else if (chdir(argList[1]) == -1)
-        fprintf(stderr,"Error: no such directory.\n"); 
-}
-
 
 int spawn(char* program, char** argList,int readPipe,int writePipe){
     pid_t childPid;
@@ -188,10 +165,41 @@ int spawn(char* program, char** argList,int readPipe,int writePipe){
     }
 }
 
+char* parseCWD(){
+    char* cwd = getcwd(NULL,0);
+    char* parsedCwd = malloc(strlen(cwd));
+    if(strncmp(cwd,home,strlen(home)) == 0){
+        parsedCwd[0]='~';
+        strcpy(parsedCwd+1,cwd+strlen(home));
+        free(cwd);
+        return parsedCwd;
+    }
+    else{
+        free(parsedCwd);
+        return cwd;
+    }    
+}
+
+int getNextPipePosition(char** argList){
+    for(int i=0; argList[i]!=NULL;i++){
+            if(strcmp(argList[i],"|") == 0){
+                return i;
+            }
+    }
+    return 0;
+}
+
+int changeDirectory(char* command, char** argList){
+    if( argList[1] == NULL || strcmp(argList[1],"~") == 0)
+        chdir(home);
+    else if (chdir(argList[1]) == -1)
+        fprintf(stderr,"Error: no such directory.\n"); 
+}
+
 void ignore (int signal_number){
     interrupted = 1;
 }
-void sigStructsInit(){
+void initializeSignalStructures(){
     struct sigaction sINT;
     memset(&sINT,0,sizeof(sINT));
     sINT.sa_handler = &ignore;
